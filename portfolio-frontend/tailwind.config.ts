f7204adb3801719447ba5713e5a32bb2d6370360
Geraldo Pereira <110185110+Geraldopereirads@users.noreply.github.com>
2023-12-29 23:34:11 -0300

    feat: first commit

---
+++ portfolio-frontend/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#D6CD2A',
+        dark: '#1B1B1B',
+        light: '#DAD7CE',
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
